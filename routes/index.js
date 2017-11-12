b7abf7fcf701efbde32500a72ca4bc23678c51a7
Johann <JohnRPB@users.noreply.github.com>
2017-11-11 19:10:15 -0800

    Add comments to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
-var express = require('express');
-var router = express.Router();
+// This is in routes/index.js
+let express = require('express');
+
+// Here we build the router
+let router = express.Router();
 const {God, gods} = require('../models/gods')
 
+// We're calling get() on the router to register the root (/) and associates;
+// these will be triggered for any incoming request matching this url
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { gods: gods });
 });
 
+// Finally, we return our router so we can use it in our app.js
 module.exports = router;
